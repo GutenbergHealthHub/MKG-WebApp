943530b7936388bf504f8c8913e3476c7a4a10f5
mbastian93 <m_bastian@outlook.de>
2024-01-04 13:54:36 +0100

    update welcome text

--- src/components/welcome/welcome.tsx
+++ src/components/welcome/welcome.tsx
@@ -13,19 +13,19 @@ export class Welcome {
         <p class="u-infotext block">
           Sehr geehrte Patientinnen und Patienten,
           <br /> Bei Ihnen ist der Ersatz eines oder beider Kiefergelenke durch eine Totalendoprothese
-          vorgesehen. Ihr individuelles Wohlbefinden steht im Mittelpunkt unseres Interesses, denn wir sind
-          bestrebt, Ihre medizinische Versorgung und Betreuung kontinuierlich zu verbessern. Diese Befragung
-          dient dazu, ein tieferes Verständnis für Ihre persönliche Wahrnehmung und Bewertung der
-          Lebensqualität zu gewinnen. Die Bedeutung des subjektiven Wohlbefindens im Gesundheitswesen ist von
-          zentraler Bedeutung. Nicht nur medizinische Faktoren, sondern auch psychologische, soziale und
-          emotionale Aspekte spielen eine Rolle für Ihre Gesundheit und Lebensqualität. Daher ist es uns ein
-          Anliegen, Ihre persönlichen Einschätzungen zu verstehen und in unsere Behandlungsansätze
-          einzubeziehen. Ihre Teilnahme an dieser Befragung ist freiwillig und Ihre Antworten werden
-          vertraulich behandelt. Die gesammelten Daten werden anonymisiert ausgewertet, sodass keine
-          Rückschlüsse auf Ihre Identität gezogen werden können. Bitte nehmen Sie sich etwas Zeit, um die
-          nachfolgenden Fragen sorgfältig zu beantworten. Ihre ehrlichen Rückmeldungen tragen dazu bei, ein
-          umfassendes Bild über Ihr Wohlbefinden zu erhalten. Wir danken Ihnen herzlich im Voraus für Ihre
-          Teilnahme und Ihr wertvolles Feedback.
+          vorgesehen bzw. durchgeführt worden. Ihr individuelles Wohlbefinden steht im Mittelpunkt unseres
+          Interesses, denn wir sind bestrebt, Ihre medizinische Versorgung und Betreuung kontinuierlich zu
+          verbessern. Diese Befragung dient dazu, ein tieferes Verständnis für Ihre persönliche Wahrnehmung
+          und Bewertung der Lebensqualität zu gewinnen. Die Bedeutung des subjektiven Wohlbefindens im
+          Gesundheitswesen ist von zentraler Bedeutung. Nicht nur medizinische Faktoren, sondern auch
+          psychologische, soziale und emotionale Aspekte spielen eine Rolle für Ihre Gesundheit und
+          Lebensqualität. Daher ist es uns ein Anliegen, Ihre persönlichen Einschätzungen zu verstehen und in
+          unsere Behandlungsansätze einzubeziehen. Ihre Teilnahme an dieser Befragung ist freiwillig und Ihre
+          Antworten werden vertraulich behandelt. Die gesammelten Daten werden anonymisiert ausgewertet,
+          sodass keine Rückschlüsse auf Ihre Identität gezogen werden können. Bitte nehmen Sie sich etwas
+          Zeit, um die nachfolgenden Fragen sorgfältig zu beantworten. Ihre ehrlichen Rückmeldungen tragen
+          dazu bei, ein umfassendes Bild über Ihr Wohlbefinden zu erhalten. Wir danken Ihnen herzlich im
+          Voraus für Ihre Teilnahme und Ihr wertvolles Feedback.
         </p>
 
         <ul class="u-list-reset">
